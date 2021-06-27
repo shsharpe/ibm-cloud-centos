13b5eb48223dfef4b50aadc7a4916f19e5cc7296
shsharpe <shawnsharpe@gmail.com>
2021-06-27 16:52:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 ##############################################################################
 provider "ibm" {
   /* Uncomment ibmcloud_api_key while testing from CLI */
-  ibmcloud_api_key      = var.api_key
+ # ibmcloud_api_key      = var.api_key
   generation            = 2 
   region                = var.region
   ibmcloud_timeout      = 300
